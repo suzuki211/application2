63387670c2d585756cf41659004e447f14782800
suzuki <s.suzukitime0211@gmail.com>
2021-06-09 14:12:53 +0000

    (Update)6/9

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,8 +7,8 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
